d7c28bc8a326bfab4d6ff29fdecc9f9193d90f8f
Nima Bullock <nbopticalgreen@gmail.com>
2020-05-09 21:45:30 -0700

    Reload sound integration + Tester sounds

---
+++ FloodedGrounds/Assets/All_Scripts/FMOD_AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Opsive.UltimateCharacterController.Events;
+
+public class FMOD_AudioManager : MonoBehaviour
+{
+    private static FMOD_AudioManager s_Instance;
+    public static FMOD_AudioManager Instance
+    {
+        get
+        {
+            if (!s_Initialized)
+            {
+                s_Instance = new GameObject("FMOD Audio Manager").AddComponent<FMOD_AudioManager>();
+                s_Initialized = true;
+            }
+            return s_Instance;
+        }
+    }
+    private static bool s_Initialized;
+
+    private void Awake()
+    {
+        EventHandler.RegisterEvent<Transform, string>("PlayFMODSound", Play);
+    }
+    public void Play(Transform location, string eventName)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(eventName, location.position);
+    }
+
+    public void OnDestroy()
+    {
+        EventHandler.UnregisterEvent<Transform, string>("PlayFMODSound", Play);
+    }
+}
